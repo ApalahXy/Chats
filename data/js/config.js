a79668eba3c3c32fd386dc55f1eb07007d84a85e
ApalahXy <155531891+ApalahXy@users.noreply.github.com>
2024-01-07 21:20:03 +0700

    Update config.js

--- data/js/config.js
+++ data/js/config.js
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
-    apiKey: "FROM YOUR FIREBASE PROJECT",
-    authDomain: "FROM YOUR FIREBASE PROJECT",
-    databaseURL: "FROM YOUR FIREBASE PROJECT",
-    projectId: "FROM YOUR FIREBASE PROJECT",
-    storageBucket: "FROM YOUR FIREBASE PROJECT",
-    messagingSenderId: "FROM YOUR FIREBASE PROJECT",
-    appId: "FROM YOUR FIREBASE PROJECT"
+  apiKey: "AIzaSyA5fTNlsrYJO1AqtPj2j0BNL5GHShyPPKM",
+  authDomain: "apps-chat-worlds.firebaseapp.com",
+  databaseURL: "https://apps-chat-worlds-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "apps-chat-worlds",
+  storageBucket: "apps-chat-worlds.appspot.com",
+  messagingSenderId: "395735317936",
+  appId: "1:395735317936:web:40d5ff89ddbea60f082a75",
+  measurementId: "G-PJCW9P4HDX"
 };
