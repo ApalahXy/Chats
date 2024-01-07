576ba9148f30bcdc6a829b671116306de1103eeb
ApalahXy <myysenzs@gmail.com>
2024-01-07 21:17:29 +0700

    first commit

---
+++ data/js/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "FROM YOUR FIREBASE PROJECT",
+    authDomain: "FROM YOUR FIREBASE PROJECT",
+    databaseURL: "FROM YOUR FIREBASE PROJECT",
+    projectId: "FROM YOUR FIREBASE PROJECT",
+    storageBucket: "FROM YOUR FIREBASE PROJECT",
+    messagingSenderId: "FROM YOUR FIREBASE PROJECT",
+    appId: "FROM YOUR FIREBASE PROJECT"
+};
